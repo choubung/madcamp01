93db3e05be0dcc4b8c684e6c9e8b7303906a9284
choubung <choubung@sookmyung.ac.kr>
2024-07-01 16:30:44 +0900

    240701 앱 아이콘 색상 변경(노랑으로)

--- app/src/main/java/com/example/myapp/ImageDetailActivity.java
+++ app/src/main/java/com/example/myapp/ImageDetailActivity.java
@@ -2,16 +2,16 @@ package com.example.myapp;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.Uri;
-
-
 import java.io.IOException;
+import java.io.InputStream;
+
 public class ImageDetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_IMAGE_URI = "com.example.myapp.EXTRA_IMAGE_URI";
@@ -25,13 +25,34 @@ public class ImageDetailActivity extends AppCompatActivity {
 
         String imageUriString = getIntent().getStringExtra(EXTRA_IMAGE_URI);
         if (imageUriString != null) {
-            Uri imageUri = Uri.parse(imageUriString);
             try {
-                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                // 이미지 URI를 Bitmap으로 변환
+                Bitmap bitmap = decodeBitmapFromFileUri(imageUriString);
+
+                // 이미지 크기를 적절히 조정
+                int maxDimension = 1024; // 최대 너비 또는 높이
+                bitmap = resizeBitmap(bitmap, maxDimension);
+
                 imageView.setImageBitmap(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private Bitmap decodeBitmapFromFileUri(String imageUriString) throws IOException {
+        InputStream inputStream = getContentResolver().openInputStream(Uri.parse(imageUriString));
+        return BitmapFactory.decodeStream(inputStream);
+    }
+
+    private Bitmap resizeBitmap(Bitmap originalBitmap, int maxSize) {
+        int width = originalBitmap.getWidth();
+        int height = originalBitmap.getHeight();
+
+        float aspectRatio = (float) width / height;
+        int newWidth = maxSize;
+        int newHeight = Math.round(newWidth / aspectRatio);
+
+        return Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
     }
 }
