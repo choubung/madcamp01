989be97ade80b8c1f9c9f0ee973d29e381ca0c7b
choubung <choubung@sookmyung.ac.kr>
2024-07-01 14:58:41 +0900

    24.07.01 연락처 DB 생성 및 삭제 기능 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -38,5 +37,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    implementation("com.google.android.material:material:1.3.*")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
 }
